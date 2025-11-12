b9857d71fee15d450d86f8b1c154330575305e09
Adi-The-Pro <adira42004@gmail.com>
2025-11-12 15:47:32 +0530

    Loop logic

---
+++ Loop.java
@@ -0,0 +1,16 @@
+public class Loop {
+
+    public static void main(String[] args){
+        int x = 10;
+        for(int i = 0; i< x ; i++){
+            System.out.println(i);
+        }
+
+        int i = 0;
+        while(i<x){
+            System.out.println(i);
+            i++;
+        }
+    }   
+
+}
